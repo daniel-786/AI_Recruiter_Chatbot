e05a3febbd5c6f93e47c7787d5699f5797f0b0cd
Muhammad Daniyal <infomuhammaddaniyal786@gmail.com>
2025-01-20 22:55:42 +0500

    Re-commit files

--- secrets_1.py
+++ secrets_1.py
@@ -1,1 +1,2 @@
+import os
 openai_api_key = "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"
