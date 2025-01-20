c45643f38b4857b88278edea33a69538d336cd7a
Muhammad Daniyal <infomuhammaddaniyal786@gmail.com>
2025-01-20 23:16:03 +0500

    Re-commit files

--- app.py
+++ app.py
@@ -26,3 +26,5 @@ user_input = st.text_input("Enter your query:")
 if st.button("Get Response"):
     response = query_gpt4(user_input)
     st.write(f"Chatbot Response: {response}")
+
+openai_api_key = st.secrets ["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"]
--- openai_api_key.py
+++ openai_api_key.py
@@ -1,2 +1,1 @@
-import streamlit as st
 openai_api_key = st.secrets ["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"]
